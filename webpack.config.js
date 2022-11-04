7431a963f418f2d600c3517766eb87ff27883e57
dulip-sameera <dsgitrepo101@gmail.com>
2022-11-04 12:36:33 +0530

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "YamiYam: Simple. Classic. Delicious.",
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: path.resolve(__dirname, "dist/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: "style-loader",
+            options: {
+              injectType: "singletonStyleTag",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[hash][exit][query]",
+        },
+      },
+    ],
+  },
+};
